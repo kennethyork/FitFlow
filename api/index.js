a5b09cdf24ab0816c9f518ffa02d1bda8ae1cd11
kennethyork <kennethhy.me@gmail.com>
2026-03-26 19:29:41 -0400

    fix: add api/package.json with type:commonjs to fix Vercel ESM conflict

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 process.env.VERCEL = '1';
 const app = require('../server/index.js');
-export default app;
+module.exports = app;
